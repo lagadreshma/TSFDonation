efac9e3523f790c8c91d4208a30b188e57ddb990
Lagad Reshma <96974585+lagadreshma@users.noreply.github.com>
2022-01-18 11:13:12 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,32 +1,45 @@
-<?php
+<html>
    
-  if(isset($_POST["submit"])){
-     $username = $_POST["username"];
-     $email = $_POST["email"];
-     $message = $_POST["messages"];
-
-     $to = "reshmalagad27@gmail.com";
-     $subject = "Contact with Us";
-
-     $message = "Name : {$username}
-                 Email : {$email}
-                 Message : {$message}";
-
-// Always set content-type when sending HTML email
-     $headers = "MIME-Version: 1.0" . "\r\n";
-     $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-
-// More headers
-     $headers .= 'From: reshmalagad27@gmail.com';
-     
-     $mail = mail($to,$subject,$message,$headers);
-
-     if($mail){
-       echo "<script>alert($message sent sucessfully)</script>";
+   <head>
+      <title>Sending HTML email using PHP</title>
+   </head>
+   
+   <body>
+      
+      <?php
+      if(isset($_POST["submit1"])){
+          
+          $username = $_POST["username"];
+          $email = $_POST["email"];
+          $message = $_POST["messages"];
+          
+          
+          
+          
+          
+         $to = "reshmalagad27@gmail.com";
+         $subject = "Contact with Us";
+         
+         $message =  "Name : {$username},<br>
+                     Email : {$email}
+                     <br>
+                      Message : {$message}";
+        // $message .= "<h1>This is headline.</h1>";
+         
+         $header = "From: tsfintern13@gmail.com\r\n";
+        
+         $header .= "MIME-Version: 1.0\r\n";
+         $header .= "Content-type: text/html\r\n";
+         
+         $retval = mail ($to,$subject,$message,$header);
+         
+         if( $retval == true ) {
+             echo "$message sent succesfully";
      }
-     else{
-       echo "<script>alert($message not sent)</script>";
-     }
-  }
-
-?>
+         else {
+           echo "$message not sent";
+         }}
+      ?>
+      
+   </body>
+</html>
